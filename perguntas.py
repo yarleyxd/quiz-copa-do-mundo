faf1aa823d2f9ccc10563a8e84a141226d8234ad
Yarley Aguiar <73827761+yarleyxd@users.noreply.github.com>
2026-06-18 16:19:02 -0300

    Update perguntas.py

--- perguntas.py
+++ perguntas.py
@@ -1,1 +1,52 @@
-
+perguntas = [
+{
+    "enunciado": "Qual país venceu a primeira copa do mundo em 1930?",
+    "opcoes": ["A) Brasil", "B) Uruguai", "C) Argentina", "D) Itália"],
+    "resposta": "B"
+},
+{
+    "enunciado": "Qual a seleção que possui mais títulos da Copa do Mundo?",
+    "opcoes": ["A) Alemanha", "B) Itália", "C) Brasil", "D) Argentina"],
+    "resposta": "C"
+},
+{
+    "enunciado": "Em que país foi realizada a Copa do Mundo de 2014?",
+    "opcoes": ["A) Alemanha", "B) África do Sul", "C) Rússia", "D) Brasil"],
+    "resposta": "D"
+},
+{
+    "enunciado": "Quem foi o campeão da Copa do Mundo de 2022?",
+    "opcoes": ["A) França", "B) Brasil", "C) Argentina", "D) Croácia"],
+    "resposta": "C"
+},
+{
+    "enunciado": "Quantos títulos mundiais o Brasil possui?",
+    "opcoes": ["A) 4", "B) 5", "C) 6", "D) 7"],
+    "resposta": "B"
+},
+{
+    "enunciado": "Qual jogador é conhecido como 'Rei do Futebol'?",
+    "opcoes": ["A) Maradona", "B) Pelé", "C) Messi", "D) Cristiano Ronaldo"],
+    "resposta": "B"
+},
+{
+    "enunciado": "A Copa do Mundo de 2026 será realizada em quantos países?",
+    "opcoes": ["A) 1", "B) 2", "C) 3", "D) 4"],
+    "resposta": "C"
+},
+{
+    "enunciado": "8. Quem é o jogador que marcou o gol conhecido como 'A mão de Deus'?",
+    "opcoes": ["A) Michael Platini", "B) Zico", "C) Gary Lineker", "D) Maradona"],
+    "resposta": "D"
+},
+{
+    "enunciado": "Quem marcou o gol do título da Alemanha na final da Copa de 2014?",
+    "opcoes": ["A) Klose", "B) Neuer", "C) Kroos", "D) Götze"],
+    "resposta": "D"
+},
+{
+    "enunciado": "Que país sediou a Copa do Mundo de 2022?",
+    "opcoes": ["A) Brasil", "B) Argentina", "C) Catar", "D) Emirados Árabes"],
+    "resposta": "C"
+}
+]
