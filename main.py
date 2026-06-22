4fd18f4b1165552526059e7ec07c95ecb5f7b8b5
Yarley Aguiar <73827761+yarleyxd@users.noreply.github.com>
2026-06-22 20:50:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from perguntas import perguntas
 from funcoes import *
 from ranking import *
@@ -52,41 +51,4 @@ while True:
 
     else:
         print("Opção inválida! Digite apenas 1, 2  ou 3.")
-        
-from perguntas import perguntas
-from funcoes import *
-
-while True:
-    opcao = exibir_menu() 
-    
-    if opcao == "1":
-        while True:
-            nome = input("Digite seu nome: ")
-            
-            if len(nome) >= 2:
-                break
-            print("Nome inválido! Digite pelo menos 2 caractéres.")
-
-        acertos = 0
-        print(f"\nBoa sorte, {nome}!")
-
-        for numero, pergunta in enumerate(perguntas, start = 1):
-            print(f"\nQuestão {numero}")
-
-            if fazer_pergunta(pergunta):
-                acertos += 1
-
-        print("\n" + "=" * 40)
-        print(f"Jogador: {nome}")
-        print(f"Acertos: {acertos} de {len(perguntas)}")
-        print("=" * 40)
-
-        continue 
-
-    elif opcao == "2":
-        print("\n😄 | Obrigado por jogado! Até a próxima.")
-        break
-
-    else:
-        print("Opção inválida! Digite apenas 1 ou 2.")
         
