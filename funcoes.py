f745545c8d82e02f62c168b4cd2a339748668fb0
Carlos_Italo <italo.alves00@aluno.ifce.edu.br>
2026-06-23 22:37:52 -0300

    Criação da função jogar e classificar desempenho

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,5 @@
+from ranking import adicionar_ao_ranking #ITA 23/06 - importação da função existente adicionar_ao_ranking
+
 def exibir_menu():
     """Exibe o menu inicial para o usuário
     e recebe a opção que ele escolher."""
@@ -34,3 +36,50 @@ def fazer_pergunta(pergunta):
         print("❌ | Que pena, você errou!")
         print(f"Resposta correta: {pergunta['resposta']}")
         return False
+
+# ITA 23/06 - Criada a função de desempenho do jogador de acordo com a sua pontuação.
+def classificar_desempenho(acertos):
+    if acertos == 10:
+        return "Desempenho: Perfeito! Você é um gênio!"
+    elif acertos >= 8:
+        return "Desempenho: Excelente desempenho!"
+    elif acertos >= 6:
+        return "Desempenho: Bom trabalho!"
+    elif acertos >= 4:
+        return "Desempenho: Você pode melhorar!"
+    else:
+        return "Desempenho: Continue estudando!"
+    
+
+# ITA 23/06 - Criada a função que controla a partida.
+def jogar(perguntas, ranking):
+    
+    while True:
+        nome = input("Digite seu nome: ")
+        
+        if len(nome) >= 2:
+            break
+        print("Nome inválido! Digite pelo menos 2 caractéres.")
+
+    acertos = 0
+    print(f"\nBoa sorte, {nome}!")
+
+    for numero, pergunta in enumerate(perguntas, start = 1):
+        print(f"\nQuestão {numero}")
+
+        if fazer_pergunta(pergunta):
+            acertos += 1
+
+    print("\n" + "=" * 40)
+    print(f"         RESULTADO DE {nome}")
+    print("=" * 40)
+    print(f"Acertos: {acertos} de {len(perguntas)}")
+    print("=" * 40)
+
+    # Chamando a função de classificação de desempenho criada anteriormente
+    mensagem = classificar_desempenho(acertos)
+    print(mensagem)
+
+    # Adiciona o jogador atual ao ranking recebido e guarda o resultado
+    ranking_atualizado = adicionar_ao_ranking(ranking, nome, acertos)    
+    return ranking_atualizado
--- main.py
+++ main.py
@@ -2,46 +2,15 @@ from perguntas import perguntas
 from funcoes import *
 from ranking import *
 
+
 ranking_geral = []
 
 while True:
     opcao = exibir_menu() 
     
     if opcao == "1":
-        while True:
-            nome = input("Digite seu nome: ")
-            
-            if len(nome) >= 2:
-                break
-            print("Nome inválido! Digite pelo menos 2 caractéres.")
-
-        acertos = 0
-        print(f"\nBoa sorte, {nome}!")
-
-        for numero, pergunta in enumerate(perguntas, start = 1):
-            print(f"\nQuestão {numero}")
-
-            if fazer_pergunta(pergunta):
-                acertos += 1
-
-        print("\n" + "=" * 40)
-        print(f"         RESULTADO DE {nome}")
-        print("=" * 40)
-        print(f"Acertos: {acertos} de {len(perguntas)}")
-        print("=" * 40)
-
-        if acertos == 10:
-            print("Desempenho: Perfeito! Você é um gênio!")
-        elif acertos >= 8:
-            print("Desempenho: Excelente desempenho!")
-        elif acertos >= 6:
-            print("Desempenho: Bom trabalho!")
-        elif acertos >= 4:
-            print("Desempenho: Você pode melhorar!")
-        else:
-            print("Desempenho: Continue estudando!")
-
-        ranking_geral = adicionar_ao_ranking(ranking_geral, nome, acertos)    
+        # ITA 23/06 - removi alógica da partida para função jogar .
+        ranking_geral = jogar(perguntas, ranking_geral)    
 
     elif opcao == "2":
         exibir_ranking(ranking_geral)
